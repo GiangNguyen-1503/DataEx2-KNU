6e07d2a6721afeec3d9ad1f153a1c9c6fd6ba140
Giang Nguyen <giangnguyen@Giangs-MacBook-Air.local>
2025-05-17 22:42:06 +0900

    Initial commit

---
+++ Sender.java
@@ -0,0 +1,64 @@
+import java.net.*;
+import java.io.*;
+
+public class Sender {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3) {
+            System.out.println("Usage: java Sender <receiver_host> <receiver_port> <file_path>");
+            return;
+        }
+
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        String filePath = args[2];
+        File file = new File(filePath);
+        String fileName = file.getName();
+
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress receiverAddress = InetAddress.getByName(host);
+
+        //SENDING GREETING
+        byte[] greetingMsg = "Greeting".getBytes();
+        DatagramPacket greetingPacket = new DatagramPacket(greetingMsg, greetingMsg.length, receiverAddress, port);
+        socket.send(greetingPacket);
+
+        //SENDING FILE NAME
+        byte[] fileNameMsg = fileName.getBytes();
+        DatagramPacket fileNamePacket = new DatagramPacket(fileNameMsg, fileNameMsg.length, receiverAddress, port);
+        socket.send(fileNamePacket);
+
+        //RECEIVING OK
+        byte[] buffer = new byte[4096];
+        DatagramPacket okPacket = new DatagramPacket(buffer, buffer.length);
+        socket.receive(okPacket);
+        String okMsg = new String(okPacket.getData(), 0, okPacket.getLength());
+        if (!okMsg.equals("OK")) {
+            System.out.println("Haven't received OK yet");
+            socket.close();
+            return;
+        }
+
+        //SENDING FILE
+        FileInputStream fis = new FileInputStream(file);
+        int bytesRead;
+        while ((bytesRead = fis.read(buffer)) != -1) {
+            DatagramPacket dataPacket = new DatagramPacket(buffer, bytesRead, receiverAddress, port);
+            socket.send(dataPacket);
+        }
+        fis.close();
+
+        //SENDING FINISH
+        byte[] finishMsg = "Finish".getBytes();
+        DatagramPacket finishPacket = new DatagramPacket(finishMsg, finishMsg.length, receiverAddress, port);
+        socket.send(finishPacket);
+
+        //RECEIVING WELDONE
+        DatagramPacket donePacket = new DatagramPacket(buffer, buffer.length);
+        socket.receive(donePacket);
+        String doneMsg = new String(donePacket.getData(), 0, donePacket.getLength());
+        if (!doneMsg.equals("Welldone")) {
+            System.out.println("Haven't received Welldone yet");
+        }
+        socket.close();
+    }    
+}
